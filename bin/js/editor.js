6a763664d44a095aaabba4ff3ff774477c079fc3
TBaudon <baudon.thomas@gmail.com>
2014-07-25 23:59:51 +0200

    starting ui.

--- bin/js/editor.js
+++ bin/js/editor.js
@@ -1,8 +1,44 @@
 (function () { "use strict";
 var editor = {};
-editor.Main = function() { };
-editor.Main.main = function() {
-	window.alert("Hello");
+editor.Main = function() {
+	var _g = this;
+	this.mWin = nodejs.webkit.Window.get();
+	window.document.getElementById("windowControlMinimize").onclick = $bind(this,this.minimize);
+	window.document.getElementById("windowControlMaximize").onclick = $bind(this,this.maximize);
+	window.document.getElementById("windowControlClose").onclick = $bind(this,this.close);
+	window.document.getElementById("windowControlDebug").onclick = $bind(this,this.debug);
+	this.mWin.on("maximize",function() {
+		_g.mMaximized = true;
+	});
+	this.mWin.on("unmaximize",function() {
+		_g.mMaximized = false;
+	});
 };
+editor.Main.main = function() {
+	new editor.Main();
+};
+editor.Main.prototype = {
+	minimize: function(Dynamic) {
+		this.mWin.minimize();
+	}
+	,maximize: function(target) {
+		if(this.mMaximized) this.mWin.unmaximize(); else this.mWin.maximize();
+	}
+	,close: function(Dynamic) {
+		this.mWin.close();
+	}
+	,debug: function(Dynamic) {
+		this.mWin.showDevTools();
+	}
+};
+var nodejs = {};
+nodejs.webkit = {};
+nodejs.webkit.$ui = function() { };
+var $_, $fid = 0;
+function $bind(o,m) { if( m == null ) return null; if( m.__id__ == null ) m.__id__ = $fid++; var f; if( o.hx__closures__ == null ) o.hx__closures__ = {}; else f = o.hx__closures__[m.__id__]; if( f == null ) { f = function(){ return f.method.apply(f.scope, arguments); }; f.scope = o; f.method = m; o.hx__closures__[m.__id__] = f; } return f; }
+nodejs.webkit.$ui = require('nw.gui');
+nodejs.webkit.Menu = nodejs.webkit.$ui.Menu;
+nodejs.webkit.MenuItem = nodejs.webkit.$ui.MenuItem;
+nodejs.webkit.Window = nodejs.webkit.$ui.Window;
 editor.Main.main();
 })();
--- src/editor/Main.hx
+++ src/editor/Main.hx
@@ -1,5 +1,6 @@
 package editor;
 import js.Browser;
+import nodejs.webkit.Window;
 
 /**
  * ...
@@ -7,13 +8,49 @@ import js.Browser;
  */
 class Main
 {
+	
+	var mWin : Window;
+	var mMaximized : Bool;
+	
 	public static function main() {
-		Browser.window.alert("Hello");
+		new Main();
 	}
 
 	public function new() 
 	{
+		mWin = Window.get();
 		
+		Browser.document.getElementById('windowControlMinimize').onclick = minimize;
+		Browser.document.getElementById('windowControlMaximize').onclick = maximize;
+		Browser.document.getElementById('windowControlClose').onclick = close;
+		Browser.document.getElementById('windowControlDebug').onclick = debug;
+		
+		mWin.on('maximize', function() {
+			mMaximized = true;
+		});
+		
+		mWin.on('unmaximize', function() {
+			mMaximized = false;
+		});
+	}
+	
+	function minimize(Dynamic) {
+		mWin.minimize();
+	}
+	
+	function maximize(target : Dynamic) {
+		if (mMaximized)
+			mWin.unmaximize();
+		else 
+			mWin.maximize();
+	}
+	
+	function close(Dynamic) {
+		mWin.close();
+	}
+	
+	function debug(Dynamic) {
+		mWin.showDevTools();
 	}
 	
 }
